38840d9ff0a91c4519cc65e8738a42d4962b093f
dbarr0s <diogo.brafael@gmail.com>
2024-03-13 23:37:50 +0000

    Create README.md e TPC5 concluído

--- TPC5/README.md
+++ TPC5/README.md
@@ -7,8 +7,9 @@ Pretende-se criar uma script de Python, que **analise e efetue pedidos**, para q
 
 ## Resumo:
 O programa desenvolvido neste **TPC5** deve responder às seguintes instruções:
-- **LISTAR**: o cliente insere esta intrução e a script lista todos os **artigos da máquina e as suas informações**.
-- **MOEDA**: o cliente começa por inserir a quantia que pretende no seguinte formato, **(1e, 2e, 1c, 2c, 5c, 10c, 20c, 50c)**. A máquina, consoante o valor inserido, vai adicionando o **valor ao saldo total do cliente**.
-- **SALDO**: o cliente ao introduzir esta instrução, o programa devolve o **saldo** que o cliente tem naquele **preciso momento**.
-- **SELECIONAR**: o cliente introduz a **instrução e o ID do artigo**, que pretende comprar, e verifica se o cliente tem **saldo suficiente** para efetuar a compra. Se tiver, **retira o valor ao saldo**, se não tiver, **não finaliza a transação.**
-- **SAIR**: o programa **divulga/"devolve"** o troco ao cliente e finaliza o programa alterando o valor da **flag**.
+- **LISTAR:** o cliente insere esta intrução e a script lista todos os **artigos da máquina e as suas informações**.
+- **MOEDA:** o cliente começa por inserir a quantia que pretende no seguinte formato, **(1e, 2e, 1c, 2c, 5c, 10c, 20c, 50c)**. A máquina, consoante o valor inserido, vai adicionando o **valor ao saldo total do cliente**.
+- **SALDO:** o cliente ao introduzir esta instrução, o programa devolve o **saldo** que o cliente tem naquele **preciso momento**.
+- **SELECIONAR:** o cliente introduz a **instrução e o ID do artigo**, que pretende comprar, e verifica se o cliente tem **saldo suficiente** para efetuar a compra. Se tiver, **retira o valor ao saldo**, se não tiver, **não finaliza a transação** e altera as quantidades que existem no JSON, retirando o artigo com o ID, que foi introduzido pelo cliente.
+- **SAIR:** o programa **divulga/"devolve"** o troco ao cliente e finaliza o programa alterando o valor da **flag**.
+- **ADICIONAR:** o cliente insere o **nome do produto**, a **quantidade** e o **preço**, o sistema verifica se o **produto já existe** e adiciona na **quantidade total** desse artigo e altera o **seu preço pelo inserido**. Se o **produto não existir**, o sistema, com os dados inseridos, cria um **novo artigo** e adiciona o mesmo no **ficheiro JSON**, com todas as **informações necessárias.** 
--- TPC5/maquina.py
+++ TPC5/maquina.py
@@ -2,7 +2,7 @@ import json
 import re
 import ply.lex as lex
 
-tokens = ['LISTAR', 'SAIR', 'SALDO', 'MOEDA', 'SELECIONAR']
+tokens = ['LISTAR', 'SAIR', 'SALDO', 'MOEDA', 'SELECIONAR', 'ADICIONAR']
 
 t_ignore = ' \t\n'
 
@@ -14,11 +14,38 @@ def t_MOEDA(t):
         t.lexer.saldo += int(t.value[:-1]) / 100
     return t
 
+
+
 def t_LISTAR(t):
     r'LISTAR'
-    print("Lista de produtos disponíveis:")
+    print("Lista de produtos:")
+    print("""ID | NOME | QUANTIDADE | PREÇO""")
     for produto, detalhes in t.lexer.data.items():
-        print(f"ID: {detalhes['id']}, Nome: {produto}, Preço: {detalhes['preco']}€")
+        print(f" {detalhes['id']} | {produto} | {detalhes['quantidade']} | {detalhes['preco']}€")
+    return t
+
+def t_ADICIONAR(t):
+    r'ADICIONAR\s+\w+\s+\d+\s+\d+(\.\d+)?'  # Exemplo: ADICIONAR produto 7 1.50
+    input_parts = t.value.split()
+    produto_nome = input_parts[1]
+    quantidade = int(input_parts[2])
+    preco = float(input_parts[3])
+    
+    if produto_nome in t.lexer.data:
+        t.lexer.data[produto_nome]['quantidade'] += quantidade
+        t.lexer.data[produto_nome]['preco'] = preco
+    else:
+        novo_produto = {
+            'id': len(t.lexer.data) + 1,
+            'preco': preco,
+            'quantidade': quantidade
+        }
+        t.lexer.data[produto_nome] = novo_produto
+    print(f"{quantidade} unidades de {produto_nome} adicionadas ao estoque.")
+    
+        # Salvar os dados atualizados no arquivo JSON
+    with open("./TPC5/artigos.json", "w") as file:
+        json.dump(t.lexer.data, file, indent=4)
     return t
 
 def t_SALDO(t):
@@ -30,14 +57,25 @@ def t_SELECIONAR(t):
     r'SELECIONAR\s\d+'
     produto_id = int(t.value.split()[1])
     produto = next((detalhes for detalhes in t.lexer.data.values() if detalhes["id"] == produto_id), None)
+    for nome, detalhes in t.lexer.data.items():
+        if detalhes["id"] == produto_id:
+            nome_produto = nome
     if produto:
         if t.lexer.saldo >= produto['preco']:
-            t.lexer.saldo -= produto['preco']
-            print(f"Compra efetuada com sucesso: Produto {produto_id}")
+            if produto['quantidade'] > 0:
+                t.lexer.saldo -= produto['preco']
+                produto['quantidade'] -= 1
+                print(f"Compra efetuada com sucesso: {nome_produto}")
+            else:
+                print(f"Produto {produto_id} esgotado.")
         else:
-            print(f"Saldo insuficiente para comprar: Produto {produto_id}")
+            print(f"Saldo insuficiente para comprar: {nome_produto}")
     else:
-        print(f"Produto inexistente com ID: {produto_id}")
+        print(f"Produto inexistente com código: {produto_id}")
+        
+        # Salvar os dados atualizados no arquivo JSON
+    with open("./TPC5/artigos.json", "w") as file:
+        json.dump(t.lexer.data, file, indent=4)
     return t
 
 def t_SAIR(t):
@@ -61,14 +99,14 @@ def main():
     lexer.saldo = 0
 
     # Display initial message
-    print("Bem-vindo à máquina de venda automática.\n")
-    print("Comandos disponíveis:\n")
-    print("-> LISTAR\n")
-    print("-> SALDO\n")
-    print("-> MOEDA\n")
-    print("-> SELECIONAR\n")
-    print("-> SAIR\n")
-
+    print("Bem-vindo à máquina de venda automática.")
+    print("Comandos disponíveis:")
+    print("-> LISTAR")
+    print("-> SALDO")
+    print("-> MOEDA")
+    print("-> SELECIONAR")
+    print("-> ADICIONAR")
+    print("-> SAIR")
 
     # Main loop
     while not lexer.flag:
